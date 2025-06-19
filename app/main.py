06583b64fa0fae981b463f06f8287d8cdf6dfdfe
Rakesh <ponkamrakesh@gmail.com>
2025-06-19 15:37:28 +0530

    home.html has been modified

--- app/main.py
+++ app/main.py
@@ -1,13 +1,26 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from pathlib import Path
 
 app = FastAPI()
-
-# Templates directory
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse("home.html", {"request": request})
+
+@app.get("/tv-shows", response_class=HTMLResponse)
+async def tv_shows(request: Request):
+    return templates.TemplateResponse("tv_shows.html", {"request": request})
+
+@app.get("/movies", response_class=HTMLResponse)
+async def movies(request: Request):
+    return templates.TemplateResponse("movies.html", {"request": request})
+
+@app.get("/new-popular", response_class=HTMLResponse)
+async def new_popular(request: Request):
+    return templates.TemplateResponse("new_popular.html", {"request": request})
+
+@app.get("/my-list", response_class=HTMLResponse)
+async def my_list(request: Request):
+    return templates.TemplateResponse("my_list.html", {"request": request})
